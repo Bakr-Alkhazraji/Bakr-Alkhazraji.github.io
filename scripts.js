2caf7a6a94225c027228288b4da987ac7371e2f4
Bakr Alkhazraji <bakr.h.alkhazraji@gmail.com>
2024-01-30 12:44:33 +0300

    second and third day modification

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var i = 0;
+var txt = 'Computer Engineer!'; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("computerEng").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(function () {
+            document.getElementById("computerEng").innerHTML = ''; // Reset innerHTML
+            i = 0; // Reset index
+            typeWriter(); // Call typeWriter again
+        }, 5000); // Wait for 5 seconds
+    }
+}
+
+typeWriter()
