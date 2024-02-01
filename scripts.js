f61d81d01db901fbda582c1226ef5ab1d2fb965c
Bakr Alkhazraji <bakr.h.alkhazraji@gmail.com>
2024-02-01 13:52:50 +0300

    minor changes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var i = 0;
-var txt = 'Computer Engineer!'; /* The text */
-var speed = 90; /* The speed/duration of the effect in milliseconds */
+var txt = 'Computer Engineer!';
+var speed = 90;
 
 function typeWriter() {
     if (i < txt.length) {
@@ -9,10 +9,10 @@ function typeWriter() {
         setTimeout(typeWriter, speed);
     } else {
         setTimeout(function () {
-            document.getElementById("computerEng").innerHTML = ''; // Reset innerHTML
-            i = 0; // Reset index
-            typeWriter(); // Call typeWriter again
-        }, 5000); // Wait for 5 seconds
+            document.getElementById("computerEng").innerHTML = '';
+            i = 0;
+            typeWriter();
+        }, 5000);
     }
 }
 
